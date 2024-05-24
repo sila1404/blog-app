9ba66ab71c4badb70c1ee2a3fc197797e00639f0
sila1404 <silamany1404@gmail.com>
2024-05-24 21:41:02 +0700

    feat(register page): binding v-model

--- resources/frontend/src/views/auth/RegisterPage.vue
+++ resources/frontend/src/views/auth/RegisterPage.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { IRegisterInput } from './auth-type'
 import { RouterLink } from 'vue-router'
+
+const registerInput = ref<IRegisterInput>({
+  name: '',
+  email: '',
+  password: ''
+})
 </script>
 
 <template>
@@ -7,22 +15,27 @@ import { RouterLink } from 'vue-router'
     <div class="col-md-4"></div>
     <div class="col-md-4">
       <div class="card">
-        <div class="card-header">
-          Register Page
-        </div>
+        <div class="card-header">Register Page</div>
+        {{ registerInput }}
         <div class="card-body">
           <form action="">
             <div class="form-group">
               <label for="">Name</label>
-              <input type="text" name="" id="" class="form-control" />
+              <input type="text" v-model="registerInput.name" name="" id="" class="form-control" />
             </div>
             <div class="form-group">
               <label for="">Email</label>
-              <input type="text" name="" id="" class="form-control" />
+              <input type="text" v-model="registerInput.email" name="" id="" class="form-control" />
             </div>
             <div class="form-group">
               <label for="">Password</label>
-              <input type="text" name="" id="" class="form-control" />
+              <input
+                type="text"
+                v-model="registerInput.password"
+                name=""
+                id=""
+                class="form-control"
+              />
             </div>
             <br />
             <RouterLink to="/">Login to account</RouterLink>
