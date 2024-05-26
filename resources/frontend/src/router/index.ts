be6b39a49b8ec276852136a8b3dcc6dee4e7ba26
sila1404 <silamany1404@gmail.com>
2024-05-26 23:21:59 +0700

    feat: create admin route

--- resources/frontend/src/router/index.ts
+++ resources/frontend/src/router/index.ts
@@ -20,14 +20,29 @@ const router = createRouter({
 				},
 			],
 		},
-		// {
-		//   path: '/about',
-		//   name: 'about',
-		//   // route level code-splitting
-		//   // this generates a separate chunk (About.[hash].js) for this route
-		//   // which is lazy-loaded when the route is visited.
-		//   component: () => import('../views/AboutView.vue')
-		// }
+		{
+		  path: '/admin',
+		  name: 'admin',
+		  component: () => import('../views/admin/AdminPage.vue'),
+			children: [
+				{
+					path: "/admin",
+					name: "dashboard",
+					component: () => import("../views/admin/DashboardPage.vue"),
+				},
+				{
+					path: "/create-post",
+					name: "create-post",
+					component: () => import("../views/admin/CreatePostsPage.vue"),
+				},
+				{
+					path: "/post-lists",
+					name: "post-lists",
+					component: () => import("../views/admin/PostListsPage.vue"),
+				},
+
+			]
+		}
 	],
 });
 
