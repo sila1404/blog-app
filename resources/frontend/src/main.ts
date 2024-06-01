c874a32633b784a8e1d0251c4ed982099e8153ce
sila1404 <silamany1404@gmail.com>
2024-06-01 20:08:18 +0700

    feat: add SweetAlert component

--- resources/frontend/src/main.ts
+++ resources/frontend/src/main.ts
@@ -7,6 +7,8 @@ import App from './App.vue'
 import router from './router'
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
+import VueSweetalert2 from 'vue-sweetalert2'
+import "sweetalert2/dist/sweetalert2.min.css"
 
 const app = createApp(App)
 
@@ -18,4 +20,5 @@ const options = {
 }
 
 app.use(Toast, options)
+app.use(VueSweetalert2)
 app.mount('#app')
