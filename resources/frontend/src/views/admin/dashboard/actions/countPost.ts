e6f15098edb2d15e0e82cf1cc834b3e792e5c589
sila1404 <silamany1404@gmail.com>
2024-06-06 16:56:13 +0700

    feat(dashboard page): get total post from backend

--- resources/frontend/src/views/admin/dashboard/actions/countPost.ts
+++ resources/frontend/src/views/admin/dashboard/actions/countPost.ts
@@ -1,5 +1,19 @@
-
+import { App } from '@/shared/App'
+import { verifyToken } from '@/helper/verifyToken'
 
 export async function countPostHTTP() {
-  return 13
+  const { headers } = verifyToken()
+  // biome-ignore lint/suspicious/noAsyncPromiseExecutor: <explanation>
+  return new Promise<number>(async (resolve, reject) => {
+    const res = await fetch(`${App.apiURL}/posts`, {
+      method: 'GET',
+      headers
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+      reject(data)
+    }
+    resolve(data.total)
+  })
 }
